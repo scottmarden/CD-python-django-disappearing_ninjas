131eba4527851c9f5617e3a2136d4a08cb013aaa
Scott <marden.scott@gmail.com>
2017-06-19 21:05:26 -0700

    Finished project. Functionality in place, but may add styling later. Displays member of TMNT based on color passed to root/ninjas/<color>

--- apps/turtle_app/views.py
+++ apps/turtle_app/views.py
@@ -4,7 +4,10 @@ from __future__ import unicode_literals
 from django.shortcuts import render, HttpResponse, redirect
 
 turtles = {
-	
+	'purple':'donatello',
+	'blue': 'leonardo',
+	'orange': 'michelangelo',
+	'red': 'raphael',
 }
 
 # Create your views here.
@@ -16,4 +19,13 @@ def ninjas(request):
 
 def ninja_colors(request, color):
 	print color
-	return render(request, 'turtle_app/ninja_colors.html')
+	if color in turtles:
+		context = {
+			'image_name': turtles[color]
+			}
+	else:
+		context = {
+			'image_name': 'notapril'
+		}
+
+	return render(request, 'turtle_app/ninja_colors.html', context)
